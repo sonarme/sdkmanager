9fdc0111b5a790d3cd41b3dbf499a28f5ec79a43
Roger Chang <roger@sonar.me>
2013-04-30 18:41:41 -0400

    factual test

--- src/test/scala/me/sonar/sdkmanager/core/FactualTest.scala
+++ src/test/scala/me/sonar/sdkmanager/core/FactualTest.scala
@@ -5,6 +5,8 @@ import javax.inject.Inject
 import me.sonar.sdkmanager.model.api._
 import me.sonar.sdkmanager.model.api.FactualRequest
 import me.sonar.sdkmanager.model.api.FactualGeo
+import scala.collection.JavaConversions._
+import org.json.JSONArray
 
 class FactualTest extends SpringComponentTest {
     @Inject
@@ -26,17 +28,23 @@ class FactualTest extends SpringComponentTest {
         val factualRequest = new FactualRequest(query = "sushi")
         val sushi = factualService.getFactualPlaces(factualRequest)
         assert(sushi.getData != null)
+        assert(sushi.getData.head.get("name").toString.toLowerCase.indexOf("sushi") > -1)
 
         val request2 = new FactualRequest(query = "sushi", geo = Option(FactualGeo(40.745396, -73.983661, 1000)))
         val nycsushi = factualService.getFactualPlaces(request2)
         assert(nycsushi.getData != null)
+        assert(nycsushi.getData.head.get("locality").toString.equals("New York"))
 
         val request3 = new FactualRequest(query = "burger", filter = Option(FactualFilter(region = Option(List("MA", "NH")))))
         val manhburger = factualService.getFactualPlaces(request3)
         assert(manhburger.getData != null)
+        assert(List("MA", "NH").contains(manhburger.getData.head.get("region")))
 
         val request4 = new FactualRequest(query = "", filter = Option(FactualFilter(category = Option(List("social", "bar")))))
         val socialAndBar = factualService.getFactualPlaces(request4)
         assert(socialAndBar.getData != null)
+
+        val category = socialAndBar.getData.get(0).get("category_labels").asInstanceOf[JSONArray].get(0).asInstanceOf[JSONArray].get(0).toString.toLowerCase
+        assert(List("social", "bar").contains(category))
     }
 }
