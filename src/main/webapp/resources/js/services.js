7bbdf58dbee3307706d429dc15ae5066beb86393
Ben Kempe <ben@k2medialabs.com>
2013-04-30 19:42:42 -0400

    cleanup

--- src/main/webapp/resources/js/services.js
+++ src/main/webapp/resources/js/services.js
@@ -5,5 +5,10 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('dashboard.services', []).
-    value('version', '0.1');
+angular.module('dashboard.services', ['ngResource'])
+    .value('version', '0.1')
+    .factory('Campaigns', function ($resource) {
+        return $resource('campaigns/:campaignId.json', {}, {
+            query: {method: 'GET', params: {phoneId: 'campaigns'}, isArray: true}
+        })
+    });
