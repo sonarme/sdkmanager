de8c9c1c937488c3bfe5e925799c43c1557c4d36
Ben Kempe <ben@k2medialabs.com>
2013-05-01 16:21:41 -0400

    cleanup

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('dashboard', ['ui.bootstrap', 'dashboard.filters', 'dashboard.services', 'dashboard.directives', 'dashboard.controllers']).
+angular.module('dashboard', ['ui', 'ui.bootstrap', 'dashboard.filters', 'dashboard.services', 'dashboard.directives', 'dashboard.controllers']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/marketingBuild', {templateUrl: 'partials/marketingBuild.html', controller: 'MarketingBuild'})
