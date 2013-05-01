46e389193d8f42e8d67d97606d9474ff6b481c52
Ben Kempe <ben@k2medialabs.com>
2013-05-01 15:51:32 -0400

    cleanup

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -4,8 +4,22 @@
 
 angular.module('dashboard.controllers', [])
     .controller('MarketingBuild', ['$scope', 'Campaign', function ($scope, Campaign) {
+        $scope.predicates = ['and', 'or']
+        $scope.predicateIdx = 0
+        $scope.attributes = [
+            {name: 'Gender', id: 'gender'},
+            {name: 'Income', id: 'income'}
+        ];
+        $scope.compareOps = [
+            {name: 'equals', id: 'EQUAL'},
+            {name: 'does not equal', id: 'NOTEQUAL'},
+            {name: 'less than', id: 'LESS'},
+            {name: 'less than or equal', id: 'LESSOREQUAL'},
+            {name: 'greater than', id: 'GREATER'},
+            {name: 'greater than or equal', id: 'GREATEROREQUAL'}
+        ];
         $scope.clauses = [
-            {}
+            {attribute: "", compareOp: ""}
         ];
         function _fn_error(err) {
             alert(err);
@@ -15,8 +29,13 @@ angular.module('dashboard.controllers', [])
         function _fn_success_put_post(data) {
         }
 
+        $scope.togglePredicate = function () {
+            $scope.predicateIdx = ($scope.predicateIdx + 1) % $scope.predicates.length
+        }
+
         $scope.save = function () {
             var campaign = new Campaign($scope.campaign);
+            campaign.clauses = $scope.clauses;
             campaign.$save();
         };
 
