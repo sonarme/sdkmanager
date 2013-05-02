e70d1985b25a3499b9491fada1204304f53a9940
Ben Kempe <ben@k2medialabs.com>
2013-05-02 16:57:59 -0400

    cleanup

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -10,6 +10,24 @@ angular.module('dashboard.controllers', [])
             {name: 'Gender', id: 'gender'},
             {name: 'Income', id: 'income'}
         ];
+        $scope.dows = [
+            {name: 'Monday', id: 2},
+            {name: 'Tuesday', id: 3},
+            {name: 'Wednesday', id: 4},
+            {name: 'Thursday', id: 5},
+            {name: 'Friday', id: 6},
+            {name: 'Saturday', id: 7},
+            {name: 'Sunday', id: 1}
+        ];
+        $scope.frequencyCaps = [
+            {name: 'per hour', id: 'hour'},
+            {name: 'per day', id: 'day'},
+            {name: 'per week', id: 'week'},
+            {name: 'per month', id: 'month'},
+            {name: 'ever', id: 'ever'}
+        ];
+        $scope.frequencyCap = $scope.frequencyCaps[$scope.frequencyCaps.length - 1];
+        $scope.frequencyCapEnabled = true;
         $scope.compareOps = [
             {name: 'equals', id: 'EQUAL'},
             {name: 'does not equal', id: 'NOTEQUAL'},
@@ -29,12 +47,17 @@ angular.module('dashboard.controllers', [])
         function _fn_success_put_post(data) {
         }
 
+        $scope.setPredicate = function (pred) {
+            $scope.predicate = pred;
+        };
         $scope.save = function () {
             var campaign = new Campaign($scope.campaign);
             campaign.clauses = $scope.clauses;
             campaign.$save();
         };
-
+        $scope.removeClause = function (clause) {
+            $scope.clauses.splice($scope.clauses.indexOf(clause), 1);
+        }
         $scope.createClause = function () {
             $scope.clauses.push({});
         }
--- src/main/webapp/resources/js/directives.js
+++ src/main/webapp/resources/js/directives.js
@@ -8,4 +8,45 @@ angular.module('dashboard.directives', [])
         return function (scope, elm, attrs) {
             elm.text(version);
         };
-    }]);
+    }])
+
+    .directive('dropdownToggleKeep',
+        ['$document', '$location', '$window', function ($document, $location, $window) {
+            var openElement = null,
+                closeMenu = angular.noop;
+            return {
+                restrict: 'CA',
+                link: function (scope, element, attrs) {
+                    scope.$watch('$location.path', function () {
+                        closeMenu();
+                    });
+                    element.parent().bind('click', function () {
+                        //closeMenu();
+                    });
+                    element.bind('click', function (event) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        var elementWasOpen = (element === openElement);
+                        if (!!openElement) {
+                            closeMenu();
+                        }
+                        if (!elementWasOpen) {
+                            element.parent().addClass('open');
+                            openElement = element;
+                            closeMenu = function (event) {
+                                if (event) {
+                                    event.preventDefault();
+                                    event.stopPropagation();
+                                }
+                                //$document.unbind('click', closeMenu);
+                                element.parent().removeClass('open');
+                                closeMenu = angular.noop;
+                                openElement = null;
+                            };
+                            //$document.bind('click', closeMenu);
+                        }
+                    });
+                }
+            };
+        }]);
+
