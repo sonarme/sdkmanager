e990393f79da8e9605e2fc4b9d4b8d5219a1c523
Roger Chang <roger@sonar.me>
2013-05-08 10:40:56 -0400

    fix test

--- src/test/scala/me/sonar/sdkmanager/core/GeofenceListTest.scala
+++ src/test/scala/me/sonar/sdkmanager/core/GeofenceListTest.scala
@@ -4,13 +4,15 @@ import me.sonar.sdkmanager.SpringComponentTest
 import javax.inject.Inject
 import collection.JavaConversions._
 import me.sonar.sdkmanager.model.db.{PlaceType, Place, GeofenceList}
+import java.util.Date
 
 class GeofenceListTest extends SpringComponentTest {
     @Inject
     var geofenceListService: GeofenceListService = _
 
     "GeofenceList" should "save a list with places" in {
-        val geofenceList = GeofenceList("testApp",
+        val appId = "testApp-" + new Date().getTime
+        val geofenceList = GeofenceList(appId,
             "NYC McDonalds",
             List[Place](
                 Place(PlaceType.factual + "-1", "mcd1", 40, -73, PlaceType.factual),
@@ -19,7 +21,7 @@ class GeofenceListTest extends SpringComponentTest {
         )
         geofenceListService.save(geofenceList)
 
-        val geofenceLists = geofenceListService.findByAppId("testApp")
+        val geofenceLists = geofenceListService.findByAppId(appId)
         assert(geofenceLists.size === 1)
         assert(geofenceLists.head.places.size() === 3)
     }
